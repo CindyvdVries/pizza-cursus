e0b3db67d99a736c9add51af0531d4ca5cfccfb4
CindyvdVries <cindyvdvries@gmail.com>
2018-10-28 22:04:02 +0100

    added map

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-mapbox
+      access-token="pk.eyJ1Ijoic2lnZ3lmIiwiYSI6Il8xOGdYdlEifQ.3-JZpqwUa3hydjAJFXIlMA"
+      map-style="mapbox://styles/mapbox/dark-v9"
+      :center="[5.673272, 52.079502]"
+      :zoom="7.88"
+      :pitch="0"
+      :bearing="0"
+      id="map"
+      ref="map"
+      >
+    </v-mapbox>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
+@import '~mapbox-gl/dist/mapbox-gl.css';
+
+html, body {
+  width: 100vw;
+  height: 100vh;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+}
+#map {
+  width: 100%;
+  height: 100%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuetify from 'vuetify';
+import Vue2MapboxGL from 'vue2mapbox-gl';
+
+Vue.use(Vue2MapboxGL);
+Vue.use(Vuetify);
+
+import 'vuetify/dist/vuetify.min.css';
 
 Vue.config.productionTip = false
 
