3737ab1f57d5459b79f8fcff1ec914d3e1da927f
CindyvdVries <cindyvdvries@gmail.com>
2018-10-29 13:46:15 +0100

    change to import vuetify

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Vue2MapboxGL from 'vue2mapbox-gl'
+import vuetify from 'vuetify'
+
+Vue.use(vuetify)
 Vue.use(Vue2MapboxGL)
 Vue.config.productionTip = false
 
--- src/plugins/vuetify.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import vuetify from 'vuetify'
-import 'vuetify/src/stylus/app.styl'
-
-Vue.use(vuetify, {
-  iconfont: 'md',
-})
