ce1f7eb7496fad91d9c5ede96f0fccae6f8db629
CindyvdVries <cindyvdvries@gmail.com>
2018-10-29 10:53:24 +0100

    use vuetify plugin

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Vuetify from 'vuetify';
-
-Vue.use(Vuetify);
-
-import 'vuetify/dist/vuetify.min.css';
 
 Vue.config.productionTip = false
 
